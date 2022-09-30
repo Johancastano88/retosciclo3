feeef2f2afc279623fe917f0d2d2eeff5947b251
Johancastano88 <johans19921@gmail.com>
2022-09-30 08:26:09 -0500

    agregando proyecto ciclo 3

---
+++ src/main/java/com/mycompany/retocliclo3/ejerciciorep.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.retocliclo3;
+
+/**
+ *
+ * @author Fabiola
+ */
+public class ejerciciorep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
